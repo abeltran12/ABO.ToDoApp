9b1f205d10fefaa6e99c528d350b7687b84421b4
Andres Jose Beltran Ortiz <batsu_75w@hotmail.com>
2024-05-03 11:28:59 -0500

    AB-feat-set default values to get all todo list

--- src/Application/ABO.ToDoApp.DIC/Models/TodoLists/SelectTodoListParameters.cs
+++ src/Application/ABO.ToDoApp.DIC/Models/TodoLists/SelectTodoListParameters.cs
@@ -2,9 +2,9 @@
 
 public class SelectTodoListParameters
 {
-    public int PageNumber { get; set; }
+    public int PageNumber { get; set; } = 1;
 
-    public int PageSize { get; set; }
+    public int PageSize { get; set; } = 10;
 
     public string Name { get; set; } = string.Empty;
 
